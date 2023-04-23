a71ff5e1ce1700d3c3fa5e1c738f910751dbe7f9
Dinitha <dinithaudayakumara0@gmail.com>
2023-04-23 10:37:04 +0530

    Table updated

--- src/components/common/ActionButton.js
+++ src/components/common/ActionButton.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function ActionButton() {
   return (
     <div>
-      <Button variant="contained">Add New</Button>
+      <Button variant="contained">Add New  </Button>
     </div>
   );
 }
